f4b8f55e90f70da2c3f5bb9309987b99c114e14d
Matthew Young <myoung.space.science@gmail.com>
2023-04-21 11:12:21 -0400

    Define basic options-reading in hybrid.c

--- src/hybrid.c
+++ src/hybrid.c
@@ -7,14 +7,60 @@ static char help[] = "A 3D hybrid particle-in-cell (PIC) simulation.";
 #include <petscdmswarm.h>
 
 
+typedef struct {
+  PetscInt nx, ny, nz;
+} Grid;
+typedef struct {
+  Grid grid;
+} UserContext;
+
+
+static PetscErrorCode ProcessOptions(UserContext *options)
+{
+  /* This could read all options from the command line, but we may want to use
+  libconfig to read most of the options from a user-provided file, and only
+  support a few runtime CLI options (e.g., output directory). Note that the
+  third argument to PetscInitialize is the name of an options-database file, so
+  it may be possible to specify all runtime options that way. */
+
+  PetscFunctionBeginUser;
+
+  PetscInt intArg;
+  PetscReal realArg;
+  PetscBool found;
+
+  options->grid.nx = 7;
+  options->grid.ny = 7;
+  options->grid.nz = 7;
+
+  PetscCall(PetscOptionsGetInt(NULL, NULL, "--nx", &intArg, &found));
+  if (found) {
+    options->grid.nx = &intArg;
+  }
+  PetscCall(PetscOptionsGetInt(NULL, NULL, "--ny", &intArg, &found));
+  if (found) {
+    options->grid.ny = &intArg;
+  }
+  PetscCall(PetscOptionsGetInt(NULL, NULL, "--nz", &intArg, &found));
+  if (found) {
+    options->grid.nz = &intArg;
+  }
+
+  PetscFunctionReturn(PETSC_SUCCESS);
+}
+
+
 int main(int argc, char **args)
 {
+  UserContext user;
+
   PetscFunctionBeginUser;
 
   // Initialize PETSc and MPI.
   PetscCall(PetscInitialize(&argc, &args, (char *)0, help));
 
   // Assign parameter values from user arguments or defaults.
+  PetscCall(ProcessOptions(&user));
 
   // Set initial particle positions and velocities.
 
