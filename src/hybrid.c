1d6e8cabf4b82f7c1d10ef1dc7ea61361db2154d
Matthew Young <myoung.space.science@gmail.com>
2023-05-02 15:05:58 -0400

    Define Boltzmann constant

--- src/hybrid.c
+++ src/hybrid.c
@@ -10,9 +10,10 @@ static char help[] = "A 3D hybrid particle-in-cell (PIC) simulation.";
 
 
 #define NDIM 3
-#define Q 1.6022e-19  // fundamental charge in C
-#define MP 1.6726e-27 // proton mass in kg
-#define ME 9.1094e-31 // electron mass in kg
+#define Q 1.6022e-19   // fundamental charge in C
+#define MP 1.6726e-27  // proton mass in kg
+#define ME 9.1094e-31  // electron mass in kg
+#define KB  1.3807e-23 // Boltzmann constant
 
 // Temporary declaration of number of particles per cell, per dimension.
 #define NPPCELL 1
