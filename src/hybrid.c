294a8bd5bee9693dcac777ce61e9e545d68203f6
Matthew Young <myoung.space.science@gmail.com>
2023-05-04 10:30:25 -0400

    WIP: Debugging statements in CollectParticles

--- src/hybrid.c
+++ src/hybrid.c
@@ -1022,6 +1022,27 @@ CollectParticles(Context *ctx)
     array[izl][iyh][ixl].n += lhl;
     array[izh][iyl][ixl].n += hll;
     array[izl][iyl][ixl].n += lll;
+    PetscCall(PetscPrintf(PETSC_COMM_WORLD, "Normalized positions: (%3.1f, %3.1f, %3.1f)\n", r.x, r.y, r.z));
+    PetscCall(PetscPrintf(PETSC_COMM_WORLD, "Physical positions:   (%3.1f, %3.1f, %3.1f)\n", x, y, z));
+    PetscCall(PetscPrintf(PETSC_COMM_WORLD, "Lower indices:        (%03d, %03d, %03d)\n", ixl, iyl, izl));
+    PetscCall(PetscPrintf(PETSC_COMM_WORLD, "Upper indices:        (%03d, %03d, %03d)\n", ixh, iyh, izh));
+    PetscCall(PetscPrintf(PETSC_COMM_WORLD, "Weight hhh:           (%03d, %03d, %03d) = %3.1f\n", ixh, iyh, izh, hhh));
+    PetscCall(PetscPrintf(PETSC_COMM_WORLD, "Weight lhh:           (%03d, %03d, %03d) = %3.1f\n", ixh, iyh, izl, lhh));
+    PetscCall(PetscPrintf(PETSC_COMM_WORLD, "Weight hlh:           (%03d, %03d, %03d) = %3.1f\n", ixh, iyl, izh, hlh));
+    PetscCall(PetscPrintf(PETSC_COMM_WORLD, "Weight llh:           (%03d, %03d, %03d) = %3.1f\n", ixh, iyl, izl, llh));
+    PetscCall(PetscPrintf(PETSC_COMM_WORLD, "Weight hhl:           (%03d, %03d, %03d) = %3.1f\n", ixl, iyh, izh, hhl));
+    PetscCall(PetscPrintf(PETSC_COMM_WORLD, "Weight lhl:           (%03d, %03d, %03d) = %3.1f\n", ixl, iyh, izl, lhl));
+    PetscCall(PetscPrintf(PETSC_COMM_WORLD, "Weight hll:           (%03d, %03d, %03d) = %3.1f\n", ixl, iyl, izh, hll));
+    PetscCall(PetscPrintf(PETSC_COMM_WORLD, "Weight lll:           (%03d, %03d, %03d) = %3.1f\n", ixl, iyl, izl, lll));
+    PetscCall(PetscPrintf(PETSC_COMM_WORLD, "                 array[%03d][%03d][%03d].n = %3.1f\n", izh, iyh, ixh, array[izh][iyh][ixh].n));
+    PetscCall(PetscPrintf(PETSC_COMM_WORLD, "                 array[%03d][%03d][%03d].n = %3.1f\n", izl, iyh, ixh, array[izl][iyh][ixh].n));
+    PetscCall(PetscPrintf(PETSC_COMM_WORLD, "                 array[%03d][%03d][%03d].n = %3.1f\n", izh, iyl, ixh, array[izh][iyl][ixh].n));
+    PetscCall(PetscPrintf(PETSC_COMM_WORLD, "                 array[%03d][%03d][%03d].n = %3.1f\n", izl, iyl, ixh, array[izl][iyl][ixh].n));
+    PetscCall(PetscPrintf(PETSC_COMM_WORLD, "                 array[%03d][%03d][%03d].n = %3.1f\n", izh, iyh, ixl, array[izh][iyh][ixl].n));
+    PetscCall(PetscPrintf(PETSC_COMM_WORLD, "                 array[%03d][%03d][%03d].n = %3.1f\n", izl, iyh, ixl, array[izl][iyh][ixl].n));
+    PetscCall(PetscPrintf(PETSC_COMM_WORLD, "                 array[%03d][%03d][%03d].n = %3.1f\n", izh, iyl, ixl, array[izh][iyl][ixl].n));
+    PetscCall(PetscPrintf(PETSC_COMM_WORLD, "                 array[%03d][%03d][%03d].n = %3.1f\n", izl, iyl, ixl, array[izl][iyl][ixl].n));
+    PetscCall(PetscPrintf(PETSC_COMM_WORLD, "\n"));
     // Assign flux values (first moments wrt velocity).
     v = vel[ip];
     w[0] = v.x;
