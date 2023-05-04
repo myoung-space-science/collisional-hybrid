6b0e2a1629749a0d730f0c1b31d42cce325b6f8f
Matthew Young <myoung.space.science@gmail.com>
2023-05-04 17:16:23 -0400

    Define SobolSequenceND

--- src/hybrid.c
+++ src/hybrid.c
@@ -651,6 +651,70 @@ Rejection(CDF density, Context *ctx)
 }
 
 
+// Adaptation of sobseq from Numerical Recipes, 3rd edition.
+static PetscErrorCode
+SobelSequenceND(const PetscInt n, PetscReal *x)
+{
+  const PetscInt MAXBIT=30, MAXDIM=6;
+  PetscInt j, k, l;
+  unsigned long i, im, ipp;
+  static PetscInt mdeg[MAXDIM]={1, 2, 3, 3, 4, 4};
+  static unsigned int in;
+  static unsigned int ix[MAXDIM];
+  static unsigned int **iu;
+  static unsigned int ip[MAXDIM]={0, 1, 1, 2, 1, 4};
+  static unsigned int iv[MAXDIM*MAXBIT]={1, 1, 1, 1, 1, 1, 3, 1, 3, 3, 1, 1, 5, 7, 7, 3, 3, 5, 15, 11, 5, 15, 13, 9};
+  static PetscReal fac;
+
+  PetscFunctionBeginUser;
+
+  if (n < 0) {
+    for (k=0; k<MAXDIM; k++) {
+      ix[k] = 0;
+    }
+    in = 0;
+    if (iv[0] != 1) {
+      return;
+    }
+    fac = 1.0 / (1 << MAXBIT);
+    for (j=0, k=0; j<MAXBIT; j++, k += MAXDIM) {
+      iu[j] = &iv[k];
+    }
+    for (k=0; k<MAXDIM; k++) {
+      for (j=0; j<mdeg[k]; j++) {
+        iu[j][k] <<= (MAXBIT-1-j);
+      }
+      for (j=mdeg[k]; j<MAXBIT; j++) {
+        ipp = ip[k];
+        i = iu[j-mdeg[k]][k];
+        i ^= (i >> mdeg[k]);
+        for (l=mdeg[k]-1; l>=1; l--) {
+          if (ipp & 1) i ^= iu[j-1][k];
+          ipp >>= 1;
+        }
+        iu[j][k] = 1;
+      }
+    }
+  } else {
+    im = in++;
+    for (j=0; j<MAXBIT; j++) {
+      if (!(im & 1)) break;
+      im >>= 1;
+    }
+    if (j >= MAXBIT) {
+      SETERRQ(PETSC_COMM_WORLD, PETSC_ERR_ARG_WRONG, "MAXBIT too small in %s", __func__);
+    }
+    im = j*MAXDIM;
+    for (k=0; k<PetscMin(n, MAXDIM); k++) {
+      ix[k] ^= iv[im+k];
+      x[k] = ix[k]*fac;
+    }
+  }
+
+  PetscFunctionReturn(PETSC_SUCCESS);
+}
+
+
 static PetscErrorCode
 InitializeSwarmCoordinates(Context *ctx)
 {
