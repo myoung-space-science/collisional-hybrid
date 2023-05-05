ee160950ad80466131bdc38b98d2f9db581f7261
Matthew Young <myoung.space.science@gmail.com>
2023-05-04 21:16:13 -0400

    SobolSequenceND: More updates and a bug fix

--- src/hybrid.c
+++ src/hybrid.c
@@ -649,22 +649,22 @@ Rejection(CDF density, Context *ctx)
 
 // Adaptation of sobseq from Numerical Recipes, 2nd edition.
 static PetscErrorCode
-SobolSequenceND(PetscInt n, PetscReal x[])
+SobolSequenceND(PetscInt *n, PetscReal x[])
 {
   PetscInt j, k, l;
   unsigned long i, im, ipp;
   static unsigned long in;
   static unsigned long ix[SOBOL_MAXDIM+1];
-  static unsigned long *iu[SOBOL_MAXDIM+1];
-  static PetscInt mdeg[SOBOL_MAXDIM+1]={0, 1, 2, 3, 3, 4, 4};
+  static unsigned long *iu[SOBOL_MAXBIT+1];
+  static unsigned long mdeg[SOBOL_MAXDIM+1]={0, 1, 2, 3, 3, 4, 4};
   static unsigned long ip[SOBOL_MAXDIM+1]={0, 0, 1, 1, 2, 1, 4};
   static unsigned long iv[SOBOL_MAXDIM*SOBOL_MAXBIT+1]={0, 1, 1, 1, 1, 1, 1, 3, 1, 3, 3, 1, 1, 5, 7, 7, 3, 3, 5, 15, 11, 5, 15, 13, 9};
   static PetscReal fac;
 
   PetscFunctionBeginUser;
 
-  if (n < 0) {
-    for (k=0; k<SOBOL_MAXDIM; k++) {
+  if (*n < 0) {
+    for (k=1; k<=SOBOL_MAXDIM; k++) {
       ix[k] = 0;
     }
     in = 0;
@@ -687,7 +687,7 @@ SobolSequenceND(PetscInt n, PetscReal x[])
           if (ipp & 1) i ^= iu[j-1][k];
           ipp >>= 1;
         }
-        iu[j][k] = 1;
+        iu[j][k] = i;
       }
     }
   } else {
@@ -700,7 +700,7 @@ SobolSequenceND(PetscInt n, PetscReal x[])
       SETERRQ(PETSC_COMM_WORLD, PETSC_ERR_ARG_WRONG, "SOBOL_MAXBIT too small in %s", __func__);
     }
     im = (j-1)*SOBOL_MAXDIM;
-    for (k=1; k<=PetscMin(n, SOBOL_MAXDIM); k++) {
+    for (k=1; k<=PetscMin(*n, SOBOL_MAXDIM); k++) {
       ix[k] ^= iv[im+k];
       x[k] = ix[k]*fac;
     }
