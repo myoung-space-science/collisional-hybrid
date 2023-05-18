e2f89ff267c9a897977b018732e30234a93f8169
Matthew Young <myoung.space.science@gmail.com>
2023-05-18 13:08:32 -0400

    Extract hybrid.h from hybrid.c

--- src/hybrid.c
+++ src/hybrid.c
@@ -7,26 +7,8 @@ static char help[] = "A 3D hybrid particle-in-cell (PIC) simulation.";
 #include <petscdmda.h>
 #include <petscdmswarm.h>
 #include <petscviewerhdf5.h>
+#include "hybrid.h"
 #include "random.h"
-
-#define ECHO_FUNCTION_ENTER {PetscCall(PetscPrintf(PETSC_COMM_WORLD, "\n--> Entering %s(...) <--\n\n", __func__));}
-#define ECHO_FUNCTION_EXIT {PetscCall(PetscPrintf(PETSC_COMM_WORLD, "\n--> Exiting %s(...) <--\n\n", __func__));}
-
-// [DEV] Not in use. These will create a lot of output if used in a function
-// that gets called often (e.g., an initial density function or random-number
-// generator).
-#define FUNCTION_START { \
-  PetscFunctionBeginUser; \
-  PetscCall(PetscPrintf(PETSC_COMM_WORLD, "\n--> Entering %s(...) <--\n\n", __func__)); \
-}
-#define FUNCTION_END { \
-  PetscCall(PetscPrintf(PETSC_COMM_WORLD, "\n--> Exiting %s(...) <--\n\n", __func__)); \
-  PetscFunctionReturn(PETSC_SUCCESS); \
-}
-
-#define PRINT_WORLD(...) {PetscCall(PetscPrintf(PETSC_COMM_WORLD, __VA_ARGS__));}
-#define PRINT_RANKS(...) {PetscCall(PetscSynchronizedPrintf(PETSC_COMM_WORLD, __VA_ARGS__)); PetscCall(PetscSynchronizedFlush(PETSC_COMM_WORLD, PETSC_STDOUT));}
-#define NEWLINE {PRINT_WORLD("\n");}
 
 #define NDIM 3
 #define Q 1.6022e-19   // fundamental charge in C
