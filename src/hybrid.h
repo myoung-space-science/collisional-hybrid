cbfcd8d2cd45334fa95511d6fba72b730a34fd68
Matthew Young <myoung.space.science@gmail.com>
2023-05-22 10:31:39 -0400

    Add constants and typedefs to hybrid.h

--- src/hybrid.h
+++ src/hybrid.h
@@ -22,4 +22,123 @@
 #define PRINT_RANKS(...) {PetscCall(PetscSynchronizedPrintf(PETSC_COMM_WORLD, __VA_ARGS__)); PetscCall(PetscSynchronizedFlush(PETSC_COMM_WORLD, PETSC_STDOUT));}
 #define NEWLINE {PRINT_WORLD("\n");}
 
+#define NDIM 3
+#define Q 1.6022e-19   // fundamental charge in C
+#define MP 1.6726e-27  // proton mass in kg
+#define ME 9.1094e-31  // electron mass in kg
+#define KB  1.3807e-23 // Boltzmann constant
+
+// Temporary declaration of number of particles per cell, per dimension.
+#define NPPCELL 1
+// Temporary declaration of total number of particles per distribution.
+#define NPTOTAL 32768
+
+// The number of non-null values in the LHS-operator matrix stencil. This is
+// equivalent to the standard box stencil with the corners removed.
+#define NVALUES 19
+
+typedef struct {
+  PetscInt x; // x component
+  PetscInt y; // y component
+  PetscInt z; // z component
+} IntVector;
+
+typedef struct {
+  PetscReal x; // x component
+  PetscReal y; // y component
+  PetscReal z; // z component
+  PetscReal r; // magnitude
+} RealVector;
+
+typedef struct {
+  IntVector  N;  // number of cells
+  RealVector L;  // physical length
+  RealVector d;  // physical cell spacing
+  RealVector p0; // lower physical bound
+  RealVector p1; // upper physical bound
+} Grid;
+
+typedef struct {
+  PetscReal  q;     // charge
+  PetscReal  m;     // mass
+  PetscReal  nu;    // frequency of collisions with neutral particles
+  RealVector Omega; // gyrofrequency components
+  RealVector kappa; // magnetization components
+  RealVector v0;    // drift velocity
+  RealVector vT;    // thermal velocity
+  PetscReal  gamma; // thermal coefficient
+  PetscReal  T;     // temperature
+} Charged;
+
+typedef struct {
+  PetscReal  m;  // mass
+  RealVector v0; // drift velocity
+  PetscReal  vT; // thermal velocity
+  PetscReal  T;  // temperature
+} Neutral;
+
+typedef struct {
+  RealVector B0; // constant magnetic-field amplitude
+  RealVector E0; // constant electric-field amplitude
+  PetscInt   Np; // number of charged particles
+} Plasma;
+
+typedef struct {
+  PetscMPIInt rank; // global processor number
+  PetscMPIInt size; // total number of processors
+} MPIContext;
+
+extern const char *RHSTypes[];
+
+typedef enum {
+  RHS_CONSTANT,
+  RHS_SINUSOIDAL,
+  RHS_FULL,
+} RHSType;
+
+extern const char *LHSTypes[];
+
+typedef enum {
+  LHS_IDENTITY,
+  LHS_LAPLACIAN,
+  LHS_FULL,
+} LHSType;
+
+extern const char *DensityTypes[];
+
+typedef enum {
+  DENSITY_FLAT_SOBOL,
+  DENSITY_FLAT_REVERSE,
+  DENSITY_FLAT_NORMAL,
+  DENSITY_UNIFORM,
+  DENSITY_SINUSOIDAL,
+  DENSITY_GAUSSIAN,
+} DensityType;
+
+typedef struct {
+  Grid        grid;         // grid information
+  Charged     electrons;    // electron parameter values
+  Charged     ions;         // ion parameter values
+  Neutral     neutrals;     // neutral-particle parameter values
+  Plasma      plasma;       // plasma information
+  MPIContext  mpi;          // MPI information
+  Vec         vlasov;       // full vector of all Vlasov quantities
+  DM          swarm;        // PIC-swarm data manager
+  Vec         phi;          // electrostatic potential
+  Vec         rhs;          // potential-equation forcing vector
+  PetscBool   viewLHS;      // option to view LHS operator structure
+  RHSType     rhsType;      // type of RHS vector to use
+  LHSType     lhsType;      // type of LHS operator to use
+  DensityType densityType;  // type of initial density profile to use
+  PetscInt    Nt;           // number of time steps
+  PetscReal   dt;           // time-step width
+  PetscInt    it;           // time-step counter
+  long        seed;         // random-number seed
+} Context;
+
+typedef struct {
+  PetscScalar n;          // scalar density
+  PetscScalar flux[NDIM]; // vector flux
+} GridNode;
+
 #endif // HYBRID_H
