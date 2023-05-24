f6821b4c73dd3eb5ac733e82be3e69c5a5f2004e
Matthew Young <myoung.space.science@gmail.com>
2023-05-24 17:29:00 -0400

    Add plotting to write_hdf5.py

--- scripts/write_hdf5.py
+++ scripts/write_hdf5.py
@@ -3,6 +3,9 @@ import pathlib
 import typing
 
 import h5py
+import matplotlib.axes
+import matplotlib.pyplot as plt
+import matplotlib.ticker as tck
 import numpy
 
 
@@ -137,6 +140,7 @@ def write_arrays(
 ) -> None:
     """Write the computed arrays to disk."""
     path = filepath.with_suffix('.h5')
+    filestem = path.stem
     with h5py.File(path, 'w') as f:
         arrays = f.create_group('arrays')
         for name, array in vlasov.items():
@@ -146,8 +150,54 @@ def write_arrays(
             for k, v in opts.items():
                 dset.attrs[k] = v
             print(dset)
+            plotname = f"{filestem}-{name}".replace(' ', '_')
+            plotpath = path.with_name(plotname).with_suffix('.png')
+            create_figure(array)
+            plt.savefig(plotpath)
+            if verbose:
+                print(f"Saved {plotpath}")
+            plt.close()
     if dset:
         raise IOError("Dataset was not properly closed.")
+
+
+def create_figure(array: numpy.ndarray):
+    """Plot 2D planes from the given array."""
+    fig, axs = plt.subplots(
+        nrows=2,
+        ncols=2,
+        sharex='col',
+        sharey='row',
+        squeeze=False,
+        figsize=(6, 6),
+    )
+    axs[0, 1].remove()
+    nx, ny, nz = array.shape
+    axes = {
+        ('x', 'y'): {'axis': axs[0, 0], 'data': array[:, :, nz//2]},
+        ('x', 'z'): {'axis': axs[1, 0], 'data': array[:, ny//2, :]},
+        ('y', 'z'): {'axis': axs[1, 1], 'data': array[nx//2, :, :]},
+    }
+    for (xstr, ystr), current in axes.items():
+        add_plot(current['axis'], current['data'], xstr, ystr)
+    fig.tight_layout()
+
+
+def add_plot(ax: matplotlib.axes.Axes, data, xstr: str, ystr: str):
+    """Add a 2-D figure from `data` to `ax`.
+    
+    Notes
+    -----
+    This function inverts the logical y axis in order to set the origin in the
+    bottom left corner.
+    """
+    ax.pcolormesh(numpy.transpose(data))
+    ax.set_xlabel(xstr)
+    ax.set_ylabel(ystr)
+    ax.label_outer()
+    ax.set_aspect('equal')
+    ax.xaxis.set_major_locator(tck.MaxNLocator(5))
+    ax.yaxis.set_major_locator(tck.MaxNLocator(5))
 
 
 if __name__ == '__main__':
